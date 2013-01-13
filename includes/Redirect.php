140b2789036a5c5580d495b5fe49dfd236d88b9d
Emilio Cobos Álvarez <ecoal95@gmail.com>
2013-01-13 17:12:08 +0100

    Eliminado archivo innecesario

--- includes/Redirect.php
+++ includes/Redirect.php
@@ -2,14 +2,7 @@
 class Redirect {
 	public static function to($location, $status = null) {
 		if( is_int($status) ) {
-			switch ($status) {
-				case 301:
-					header("HTTP/1.1 301 Moved Permanently");
-					break;
-				case 302:
-					header("HTTP/1.1 302 Moved Temporary");
-					break;
-			}
+			Header::status($status);
 		}
 		header('Location: ' . $location);
 		exit;
